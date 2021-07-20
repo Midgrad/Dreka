edc5463b66dcedcf0a3fe536749b2c00f602ca10
MishkaRogachev <mishkarogachev@gmail.com>
2021-07-20 20:59:53 +0300

    Andd theme and theme loader for industrial-controls

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,9 +3,13 @@
 #include <QtWebEngine>
 
 #include "map_viewport_controller.h"
+#include "theme.h"
+#include "theme_activator.h"
+#include "theme_loader.h"
 
 int main(int argc, char* argv[]) {
   QCoreApplication::setOrganizationName("Midgrad");
+
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
 
@@ -17,6 +21,13 @@ int main(int argc, char* argv[]) {
   QQmlApplicationEngine engine;
   engine.rootContext()->setContextProperty("viewportController",
                                            &viewportController);
+  industrialThemeActivate(true, &engine);
+
+  ThemeLoader themeLoader;
+  themeLoader.setTheme(IndustrialTheme::instance());
+  themeLoader.setFilename("../app/theme.json");
+  themeLoader.load();
+
   engine.load(QUrl(QStringLiteral("../app/qml/MainWindow.qml")));
 
   return app.exec();
