d023535afcedca92450a73590bce78561673c660
MishkaRogachev <mishkarogachev@gmail.com>
2021-07-22 18:31:18 +0300

    Add version and git revision

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,5 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QVersionNumber>
 #include <QtWebEngine>
 
 #include "map_viewport_controller.h"
@@ -7,13 +8,20 @@
 #include "theme_activator.h"
 #include "theme_loader.h"
 
+namespace {
+const char* gitRevision = "git_revision";
+}
+
 int main(int argc, char* argv[]) {
   QCoreApplication::setOrganizationName("Midgrad");
+  QCoreApplication::setApplicationVersion(
+      QVersionNumber(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH).toString());
 
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
 
   QGuiApplication app(argc, argv);
+  app.setProperty(::gitRevision, QString(GIT_REVISION));
   QtWebEngine::initialize();
 
   MapViewportController viewportController;
