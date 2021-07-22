79997c0c3dcc98480c2ec35d74e6cc4ff7298bd2
MishkaRogachev <mishkarogachev@gmail.com>
2021-07-22 18:30:33 +0300

    Add script for ubuntu installation

---
+++ scripts/ubuntu_install_deps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt install qttools5-dev qttools5-dev-tools qtbase5-dev qtchooser qtdeclarative5-dev qtmultimedia5-dev qtquickcontrols2-5-dev libqt5serialport5-dev libqt5websockets5-dev libqt5svg5-dev libqt5opengl5-dev libqt5sql5-psql qml-module-qtquick-controls2 qml-module-qtmultimedia qml-module-qtgraphicaleffects qml-module-qtquick-layouts qml-module-qt-labs-settings qml-module-qtquick-dialogs qtwebengine5-dev qml-module-qt-labs-qmlmodels
