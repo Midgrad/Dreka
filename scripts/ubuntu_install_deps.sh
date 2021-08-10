2d4c517fe26e107286a7acbad3e2b002f3031f8e
Elias Alberts <nnhw667@gmail.com>
2021-08-10 17:20:03 +0300

    dep script fixes - added sudo check and missed dep

--- scripts/ubuntu_install_deps.sh
+++ scripts/ubuntu_install_deps.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo apt install google-mock googletest qttools5-dev qttools5-dev-tools qtbase5-dev qtchooser qtdeclarative5-dev qtmultimedia5-dev qtquickcontrols2-5-dev libqt5serialport5-dev libqt5websockets5-dev libqt5svg5-dev libqt5opengl5-dev libqt5sql5-psql qml-module-qtquick-controls2 qml-module-qtmultimedia qml-module-qtgraphicaleffects qml-module-qtquick-layouts qml-module-qt-labs-settings qml-module-qtquick-dialogs qml-module-qtwebchannel qtwebengine5-dev qml-module-qt-labs-qmlmodels
+# Запуск из-под root
+if [[ $EUID -ne 0 ]]; then
+  SUDO='sudo'
+else
+  SUDO=''
+fi
+
+$SUDO apt update -qq
+$SUDO apt install -y npm cmake google-mock googletest qttools5-dev qttools5-dev-tools qtbase5-dev qtchooser qtdeclarative5-dev qtmultimedia5-dev qtquickcontrols2-5-dev libqt5serialport5-dev libqt5websockets5-dev libqt5svg5-dev libqt5opengl5-dev libqt5sql5-psql qml-module-qtquick-controls2 qml-module-qtmultimedia qml-module-qtgraphicaleffects qml-module-qtquick-layouts qml-module-qt-labs-settings qml-module-qtquick-dialogs qml-module-qtwebchannel qtwebengine5-dev qml-module-qt-labs-qmlmodels qml-module-qtwebengine
