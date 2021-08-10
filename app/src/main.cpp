8c3e65e349e49123442943c451a904910fceebff
MishkaRogachev <mishkarogachev@gmail.com>
2021-08-10 21:00:17 +0300

    Move JSON utils to kjarni

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -14,38 +14,11 @@
 #include "map_ruler_controller.h"
 #include "map_viewport_controller.h"
 
+#include "utils.h"
+
 namespace
 {
 const char* gitRevision = "git_revision";
-
-// TODO: to utils
-void mergeJson(QJsonObject& src, const QJsonObject& other)
-{
-    for (auto it = other.constBegin(); it != other.constEnd(); ++it)
-    {
-        if (src.contains(it.key()))
-        {
-            if (src.value(it.key()).isObject() && other.value(it.key()).isObject())
-            {
-                QJsonObject one(src.value(it.key()).toObject());
-                QJsonObject two(other.value(it.key()).toObject());
-
-                mergeJson(one, two);
-                src[it.key()] = one;
-            }
-            else if (src.value(it.key()).isArray() && other.value(it.key()).isArray())
-            {
-                QJsonArray arr = other.value(it.key()).toArray();
-                QJsonArray srcArr = src.value(it.key()).toArray();
-                for (int i = 0; i < arr.size(); i++)
-                    srcArr.append(arr[i]);
-                src[it.key()] = srcArr;
-            }
-        }
-        else
-            src[it.key()] = it.value();
-    }
-}
 } // namespace
 
 int main(int argc, char* argv[])
@@ -82,7 +55,7 @@ int main(int argc, char* argv[])
     QJsonObject qmlEntries;
     for (const QString& moduleId : moduleLoader.loadedModules())
     {
-        ::mergeJson(qmlEntries, moduleLoader.module(moduleId)->qmlEntries());
+        kjarni::utils::mergeJson(qmlEntries, moduleLoader.module(moduleId)->qmlEntries());
     }
 
     engine.rootContext()->setContextProperty("qmlEntries", qmlEntries);
